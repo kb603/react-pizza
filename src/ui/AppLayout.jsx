762aa78fe48f2367237702c60ceefa0f14e686cb
kb603 <kojoaddobaffour@gmail.com>
2024-07-21 23:53:21 +0000

    updates

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,22 @@
+import { Outlet, useNavigation } from "react-router-dom";
+
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import Loader from "../ui/Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
       </main>
       <CartOverview />
-    </>
+    </div>
   );
 }
 
